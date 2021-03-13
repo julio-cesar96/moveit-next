08e6e2846c4bef0786df1d0db1dd4a8dc54b917e
julio-cesar96 <julio_cbs96@hotmail.com>
2021-03-13 12:54:27 -0300

    Corrigindo bug no reinício do contador

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -42,7 +42,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     function resetCountdown() {
         clearTimeout(countdownTimeout);
         setIsActive(false);
-        setTime(0.1 * 60);
+        setTime(30 * 60);
         setHasFinished(false);
     }
 
